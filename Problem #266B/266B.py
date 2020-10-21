ff839f7ed6d5ec182ce79606df64b19c12fe711e
Ank221199 <ankitsinha.221199@gmail.com>
2020-10-21 14:12:21 +0530

    🙃 Acceoted 266B

--- Problem #266B/266B.py
+++ Problem #266B/266B.py
@@ -2,10 +2,15 @@ a,b = map(int,input().split())
 c= input()
 list1=list(c)
 for i in range (b):
-    for j in range (a):
+    j=0
+    while(j<a):
         if (list1[j]=="B" and j<a-1):
-            tmp= list1[j]
-            list1[j]=list1[j+1]
-            list1[j+1]= tmp
+            if(list1[j+1]=="G"):
+                j = j+1
+                tmp= list1[j]
+                list1[j]=list1[j-1]
+                list1[j-1]= tmp
+        j = j+1
+            
 str1 = ''.join(list1)
 print(str1)
