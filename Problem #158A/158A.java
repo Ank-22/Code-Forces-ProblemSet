56348156be097f9b983b0fc4ab366706e29a1e5d
Keshav Agarwal <99586399+agkeshav@users.noreply.github.com>
2022-09-29 21:55:18 +0530

    Documentation added in the same file

--- Problem #158A/158A.java
+++ Problem #158A/158A.java
@@ -20,3 +20,9 @@ public class 158A{
         System.out.println(count);
     }
 }
+//Documentation for this solution
+// In this question we basically need to find out how many participants have scored more than the participant at kth position and the condition is also that the participants must have earned positive score
+// Applying brute force approach
+// We will just run a loop and iterate the array and increment the count variable whenever the score of the participant is greater than that of participant at kth postition and its score must be greater than zero.
+//Since indexing starts from zero , the score of the particpant at kth position will be arr[k-1] , we just need to compare all participants score with arr[k-1] with condition that the score should be greater than zero.
+//At the last just print the count variable and that will be our answer.
